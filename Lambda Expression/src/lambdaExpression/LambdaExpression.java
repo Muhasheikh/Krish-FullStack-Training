2515bd65fb53a0e9f64880b15d8df06995bb7d57
Muhasheikh <muhasheikh8@gmail.com>
2022-04-02 11:03:34 +0530

    Regular to Lambda Expression

---
+++ Lambda Expression/src/lambdaExpression/LambdaExpression.java
@@ -0,0 +1,29 @@
+package lambdaExpression;
+
+
+interface Square{
+	
+	public int area(int w);
+}
+public class LambdaExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Regular Expression
+		Square s = new Square() {
+			
+			@Override
+			public int area(int w) {
+				// TODO Auto-generated method stub
+				return  w * w;
+				
+			}
+		};
+		System.out.println("The area of square using regular expression "+s.area(5));
+		
+		Square s1 =(w)->w*w;
+		System.out.println("The area of square using lambda expression "+s1.area(10));
+	}
+
+}
